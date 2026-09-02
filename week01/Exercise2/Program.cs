767ec54bdf9ce7a55f56a70b925be29eed65a747
Maggie McCausland <maggiemccausland@hotmail.com>
2026-09-02 14:50:50 -0600

    completed exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.WriteLine("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        int percent = int.Parse(percentage);
+        string letterGrade = "";
+
+        if (percent >= 90)
+        { letterGrade = "A"; }
+        else if (percent >= 80)
+        { letterGrade = "B"; }
+        else if (percent >= 70)
+        { letterGrade = "C"; }
+        else if (percent >= 60)
+        { letterGrade = "D"; }
+        else
+        { letterGrade = "F"; }
+
+        Console.WriteLine($"Your grade is {letterGrade}");
+
+        if (percent >= 70)
+        { Console.WriteLine("Congrats you passed!"); }
+        else
+        { Console.WriteLine("Sorry, you did not pass. Better luck next time!"); }
     }
 }
