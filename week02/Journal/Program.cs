28e1c9199c147036263cab03e783d704a1aa6623
Maggie McCausland <maggiemccausland@hotmail.com>
2026-09-10 18:01:31 -0600

    created cs files for journal project

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
+
+        Console.Write("What would you like to do? ");
+        string userChoice = Console.ReadLine();
+        int choice = int.Parse(userChoice);
+
+        while (choice != 5) {
+            
+        }
     }
 }
