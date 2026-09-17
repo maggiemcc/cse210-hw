a6bd6ed0be93c85f5df92f98b9df70a7eae33786
Maggie McCausland <maggiemccausland@hotmail.com>
2026-09-16 18:36:15 -0600

    created basic structure for scripture memorizer classes

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Scripture
+{
+   private Reference _reference;
+   private List<Word> _words;
+
+   public Scripture (Reference reference, string text)
+    {
+
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return false;
+    }
+}
---
+++ week03/ScriptureMemorizer/Word.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public void Hide()
+    {
+        
+    }
+
+    public void Show()
+    {
+        
+    }
+
+    public bool IsHidden()
+    {
+        return false;
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+    }
+}
